c9949dc42d41c3dd6733bb20b3747bca816287fc
francesco <107208390+cestello@users.noreply.github.com>
2023-12-30 15:11:06 +0100

    login e registrazione
    controllo su db fatto

--- php/login.php
+++ php/login.php
@@ -9,13 +9,24 @@ include('../util/utils.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $email = $_POST["email"];
+    $email = $_POST["username"];
     $password = $_POST["password"];
-
     if (is_used($email))
     {
-        // TODO: check is password is correct [database]
-        echo("Accesso effettuato con successo");
+        $conn = connect_to_database();
+        $sql_query = "SELECT email FROM utenti WHERE email = '". $email . "';";
+        $query_answer = $conn->query($sql_query);
+        if($query_answer === FALSE) {
+        } else {
+            $row = $query_answer->fetch_assoc();
+            $db_password = $row["email"];
+            if($db_password == $password) {
+                echo("Accesso effettuato con successo");
+            } else {
+                echo("Password errata");
+            }
+        }
+        $conn->close();
     }
     else
     {
